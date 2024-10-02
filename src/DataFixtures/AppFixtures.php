6658b622c7546fdbcf0e0e3f613f564d4047dd10
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-10-02 21:26:34 +0200

    create fixture for episode and seasons entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Media;
+use App\Entity\Season;
+use App\Entity\Episode;
+use App\Entity\Serie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +14,44 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+
         // $product = new Product();
         // $manager->persist($product);
 
         $manager->flush();
     }
+ 
+
+
+
+
+
+    private function createEpisodes(ObjectManager $manager, Season $season)
+    {
+        for ($j = 1; $j <= 10; $j++) { 
+            $episode = new Episode();
+            $episode->setSeason($season);
+            $episode->setTitle('Episode ' . $j);
+            $episode->setDuration(45); 
+            $episode->setReleasedAt(new \DateTimeImmutable("+{$j} weeks"));
+            $manager->persist($episode);
+        }
+    }
+    private function createSeasons(ObjectManager $manager, Serie $serie) {
+        for ($seasonNumber = 1; $seasonNumber <= 5; $seasonNumber++) {
+            $season = new Season();
+            $season->setSeries($serie);
+            $season->setNumber($seasonNumber);
+
+            $manager->persist($season);
+            
+        $this->createEpisodes($manager, $season);
+    } 
+    }
+
+    
+
+
+
 }
