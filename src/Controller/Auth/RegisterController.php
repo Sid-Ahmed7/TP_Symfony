9504749839dd17c02a41cdc71eccf7c0161cb1e3
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-12-17 20:28:59 +0100

    register

--- src/Controller/Auth/RegisterController.php
+++ src/Controller/Auth/RegisterController.php
@@ -2,15 +2,41 @@
 
 namespace App\Controller\Auth;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use App\Enum\UserAccountStatusEnum;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('auth/register.html.twig');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+        
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $plainPassword = $form->get('plainPassword')->getData();
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            $user->setRoles(['ROLE_USER']);
+            $user->setAccountStatus(UserAccountStatusEnum::ACTIVE);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('auth/register.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
